f28d2ef73ad1c8b217291d4d5bbcef6a24c78f58
Ivana Yovcheva <iyovcheva@vmware.com>
2018-11-09 12:07:59 +0200

    Func 2

--- func2/handler.go
+++ func2/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. We're in function 2. Testing raw")
+	return fmt.Sprintf("Hello, Go. We're in function 2. Testing status dashboard link.")
 }
